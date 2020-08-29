fc2b60baec2538841dc00527fb6b682ca356524e
Aaryan Goel <apg5729@psu.edu>
2020-08-29 01:47:10 +0000

    wording

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 CTemp = float(input("Enter temperature in celsius: "))
-Ftemp = float(CTemp * (9/5)) + 32
+Ftemp = float((CTemp * (9/5)) + 32)
 print(str(CTemp) + "° in Celsius is equivalent to " + str(Ftemp) + "° Fahrenheit.")
